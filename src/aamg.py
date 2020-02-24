6b374de3811346e715714db83443faae56938e8f
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-02-24 01:22:39 +0100

    tests: rename legacy list ... passed directory to

--- src/aamg.py
+++ src/aamg.py
@@ -9,9 +9,9 @@ def aamg() -> None:
     '''This is the main entrypoint for our program.'''
     parser = argparse.ArgumentParser(description=
             'Get an automatically generated astrophysical model')
-    parser.add_argument('--grammar', '-g', dest='grammar', default='grammar',
+    parser.add_argument('--grammar', '-g', dest='grammar', default='GRAMMAR',
             type=str, help='the file to use as grammar input')
-    parser.add_argument('--assets', '-a', dest='assets', default='assets',
+    parser.add_argument('--assets', '-a', dest='assets', default='ASSETS',
             type=str, help='the file to use as the asset list')
     parser.add_argument('--verbose', '-v', dest='verbose', default=False,
             action='store_true', help='enable verbose')
