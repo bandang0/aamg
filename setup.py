bbb2128cd3e11f679a79271eafa0e2965f4a9702
bandang0 <duque@iap.fr>
2020-02-24 18:05:33 +0100

    infra: finish setup script for aamg-gen cli tool

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ setuptools.setup(
     long_description=long_description,
     license="MIT",
 
+    # Scripts and packages.
+    scripts=['bin/aamg-gen'],
+    packages=['aamg'],
+
     # Set our test suite so we can use `python3 setup.py test` to run tests.
     test_suite='tests.suite.aamg_test_suite',
 
