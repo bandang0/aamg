e37e7e54e1b3eec6dabbbab334a4aad6eaa79b82
Gabriel Duque <gabriel.duque@lse.epita.fr>
2020-03-04 23:12:08 +0100

    tests: switch to tox for testing

--- tests/suite.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-def aamg_test_suite():
-    '''This discovers tests automatically for us and is called by `setup.py`'''
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='*_test.py')
-    return test_suite
